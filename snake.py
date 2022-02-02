e3022a9a8ff3d0f6a7d985f93276db748efa2f90
aokiji123 <jenkaboy7@gmail.com>
2022-02-02 13:22:36 +0200

    first half

---
+++ snake.py
@@ -0,0 +1,46 @@
+from ast import While
+import pygame
+from random import randrange
+
+RES = 800
+SIZE = 50
+
+x,y = randrange(0, RES, SIZE), randrange(0, RES, SIZE)
+apple = randrange(0, RES, SIZE), randrange(0, RES, SIZE)
+snake_length = 1
+snake = [(x, y)]
+dx, dy = 0, 0
+fps = 7
+
+pygame.init()
+sc = pygame.display.set_mode([RES, RES])
+clock = pygame.time.Clock()
+
+while True:
+    sc.fill(pygame.Color('black'))
+    # drawing snake and apples
+    [(pygame.draw.rect(sc, pygame.Color('green'), (i, j, SIZE, SIZE))) for i, j in snake]
+    pygame.draw.rect(sc, pygame.Color('red'), (*apple, SIZE, SIZE))
+    # snake movement
+    x += dx * SIZE
+    y += dy * SIZE
+    snake.append((x, y))
+    snake = snake[-snake_length:]
+
+    pygame.display.flip()
+    clock.tick(fps)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    # snake control
+    key = pygame.key.get_pressed()
+    if key[pygame.K_w]:
+        dx, dy = 0, -1
+    if key[pygame.K_s]:
+        dx, dy = 0, 1
+    if key[pygame.K_a]:
+        dx, dy = -1, 0
+    if key[pygame.K_d]:
+        dx, dy = 1, 0
